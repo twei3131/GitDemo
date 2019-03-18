0bf2fb10604e5ccefb78cf1e62d6355f6de98fa8
twei3131 <848270978@qq.com>
2019-03-18 10:54:32 +0800

    测试提交

---
+++ src/com/tdu/git/Test.java
@@ -0,0 +1,12 @@
+package com.tdu.git;
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append(12);
+		System.out.print(builder.toString());
+	}
+
+}
